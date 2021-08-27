335389f0bdf8cc1f2e19988da7a1218b5e163af2
McTastic <mctastic2121@gmail.com>
2021-08-27 16:04:51 -0400

    added JS functionality to render questions to page as well as checking correct answers based on user selection

--- script.js
+++ script.js
@@ -2,131 +2,167 @@ var startBtn = document.getElementById("start");
 var timerEl = document.getElementById("time");
 var startBox = document.getElementById("start-box");
 var questionEl = document.getElementById("qBox");
+var restartBtn = document.getElementById("restart");
+var pointsEl = document.getElementById("points");
+var answersEl = document.getElementById("aBox");
+var answerA = document.getElementById("choiceA");
+var answerB = document.getElementById("choiceB");
+var answerC = document.getElementById("choiceC");
+var answerD = document.getElementById("choiceD");
+var timeLeft = 30;
+var points = 0;
+var currentQuestion = 0;
 
-function countDown() {
-    var timeLeft = 30;
-    var timeInterval = setInterval(function () {
-        if (timeLeft > 1) {
-            timerEl.textContent = timeLeft + ' seconds remaining';
-            timeLeft--;
-        } else if (timeLeft === 1) {
-            timerEl.textContent = timeLeft + ' second remaining';
-            timeLeft--;
-        } else {
-            timerEl.textContent = "Time's Up!";
-            clearInterval(timeInterval);
-        }
-    }, 1000);
+function noDelaySetInterval(func, interval) {
+  func();
+  return setInterval(func, interval);
 }
 
+function countDown(timeInterval) {
+  var timeInterval = noDelaySetInterval(function () {
+    if (timeLeft > 1) {
+      timerEl.textContent = timeLeft + " seconds remaining";
+      timeLeft--;
+    } else if (timeLeft === 1) {
+      timerEl.textContent = timeLeft + " second remaining";
+      timeLeft--;
+    } else {
+      timerEl.textContent = "Time's Up!";
+      clearInterval(timeInterval);
+      gameOver();
+    }
+  }, 1000);
+}
 
-// Question arrays  
-var questionArray = [{
-    q: "What is a question?",
+// Question arrays
+var questionArray = [
+  {
+    q: "Which is not a Javascript data type?",
+    a: ["String", "Boolean", "Noodles", "Undefined"],
+    correct: "Noodles",
+  },
+  {
+    q: "Which of the following is equal in type and value to 4?",
+    a: ["x=4", "x===4", "'4'", "x==4"],
+    correct: "x===4",
+  },
+  {
+    q: "What does DOM stand for?",
     a: [
-        "Answer1",
-        "Answer2",
-        "Answer3",
-        "Answer4",
+      "Does obstructive math",
+      "Dynamic object manipulation",
+      "Destructive old magic",
+      "Document Object Model",
     ],
-    correct: "Answer2"
-},
-{
-    q: "What is a question2?",
+    correct: "Document Object Model",
+  },
+  {
+    q: "Which of the following is a boolean?",
+    a: ["True", "29939114", "False", "Both B and D"],
+    correct: "Both B and D",
+  },
+  {
+    q: "What does HTML stand for?",
     a: [
-        "Answer1",
-        "Answer2",
-        "Answer3",
-        "Answer4",
+      "HyperText Markup Language",
+      "Hippie Trippy Monster Land",
+      "HillTop Master List",
+      "Hexidecimal Testing Mapped Language",
     ],
-    correct: "Answer2"
-},
-{
-    q: "What is a question3?",
-    a: [
-        "Answer1",
-        "Answer2",
-        "Answer3",
-        "Answer4",
-    ],
-    correct: "Answer2"
-}, {
-    q: "What is a question4?",
-    a: [
-        "Answer1",
-        "Answer2",
-        "Answer3",
-        "Answer4",
-    ],
-    correct: "Answer2"
-}, {
-    q: "What is a question5?",
-    a: [
-        "Answer1",
-        "Answer2",
-        "Answer3",
-        "Answer4",
-    ],
-    correct: "Answer2"
-}, {
-    q: "What is a question6?",
-    a: [
-        "Answer1",
-        "Answer2",
-        "Answer3",
-        "Answer4",
-    ],
-    correct: "Answer2"
-}, {
-    q: "What is a question7?",
-    a: [
-        "Answer1",
-        "Answer2",
-        "Answer3",
-        "Answer4",
-    ],
-    correct: "Answer2"
-}, {
-    q: "What is a question8?",
-    a: [
-        "Answer1",
-        "Answer2",
-        "Answer3",
-        "Answer4",
-    ],
-    correct: "Answer2"
-}, {
-    q: "What is a question9?",
-    a: [
-        "Answer1",
-        "Answer2",
-        "Answer3",
-        "Answer4",
-    ],
-    correct: "Answer2"
-}, {
-    q: "What is a question10?",
-    a: [
-        "Answer1",
-        "Answer2",
-        "Answer3",
-        "Answer4",
-    ],
-    correct: "Answer2"
-},
-]
+    correct: "HyperText Markup Language",
+  },
+  {
+    q: "Which of these is a method to add a child to a parent using Javascript?",
+    a: [".add", ".addChild", ".appendChild", ".appendToParent"],
+    correct: ".appendChild",
+  },
+  {
+    q: "Which method can be used wait for the user to click on a button?",
+    a: ["addClick", "userClick", "listendForClick", "addEventListener"],
+    correct: "addEventListener",
+  },
+  {
+    q: "What will be the result of 3+2+'7'?",
+    a: ["12", "57", "327", "Math is too hard!"],
+    correct: "57",
+  },
+  {
+    q: "Which of the following is not a type of popup box in Javascript?",
+    a: ["Notification", "Alert", "Prompt", "Confirm"],
+    correct: "Notification",
+  },
+  {
+    q: "Which of the following is used for multi-line comments in Javascript?",
+    a: ["/*", "//", "/ comment /", "./"],
+    correct: "/*",
+  },
+];
 
-// function to render question to page 
+// function to render question to page
 function renderQuestion() {
-    startBox.style.display = "none";
-    questionEl.innerHTML = questionArray[0].q;
+  questionEl.style.display = "block";
+  startBox.style.display = "none";
+//   for loop which displays question and answers
+  for (var i = 0; i < questionArray.length; i++) {
+    if (i < questionArray.length) {
+      var nextQuestion = questionArray[currentQuestion].q;
+      questionEl.textContent = nextQuestion;
+      answersEl.style.display = "flex";
+      answerA.textContent = questionArray[currentQuestion].a[0];
+      answerA.addEventListener("click", correctAnswer);
+      answerB.textContent = questionArray[currentQuestion].a[1];
+      answerB.addEventListener("click", correctAnswer);
+      answerC.textContent = questionArray[currentQuestion].a[2];
+      answerC.addEventListener("click", correctAnswer);
+      answerD.textContent = questionArray[currentQuestion].a[3];
+      answerD.addEventListener("click", correctAnswer);
+    } else {
+      gameOver();
+    }
+  }
 }
 
+function correctAnswer() {
+  var answer = questionArray[currentQuestion].correct;
+  var userChoice = this.textContent;
+  if (userChoice === answer) {
+    points += 10;
+    currentQuestion++;
+    renderQuestion();
+    
+  } else {
+    timeLeft -= 5;
+    console.log(timeLeft);
+  }
+}
+
+function restartButton() {
+  restartBtn.addEventListener("click", function () {
+    location.reload();
+  });
+}
+
+function gameOver() {
+  answersEl.style.display = "none";
+  questionEl.style.display = "none";
+  restartBtn.style.display = "block";
+  restartButton();
+}
+
+// TODO make high score page and points function correctly
+// TODO gameOver needs end state if all questions answered
+
+
+function init() {
+  restartBtn.style.display = "none";
+  pointsEl.style.display = "none";
+  answersEl.style.display = "none";
+}
 
 startBtn.addEventListener("click", function () {
-    countDown();
-    renderQuestion(); console.log(renderQuestion);
-    startBtn.disabled = true;
+  startBtn.style.display = "none";
+  countDown();
+  renderQuestion();
+});
 
-})
-
+init();
